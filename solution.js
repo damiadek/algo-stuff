994f61a6d0b1f58f6ac19349f2b2958e6070d96a
damiadek <damiadek2009@gmail.com>
2018-12-09 10:13:20 +0100

    solution

---
+++ solution.js
@@ -0,0 +1,24 @@
+function solution(N, A) {
+
+    var array = setAll(0, [], N), index = max = lastMax = 0;
+    
+    for(var i = 0; i < A.length; i++){
+        
+        index = (A[i] - 1);
+        if(A[i] <= N){
+          if(array[index] < lastMax) array[index] = lastMax;
+          array[index] += 1;
+          max = array[index] > max ? array[index] : max;
+        }else if(A[i] == N+1){
+            lastMax = max;
+        }
+    }
+    
+    for(var i = 0; i < N; i++){
+      if(array[i] < lastMax) array[i] = lastMax;
+    }
+    
+    return array;
+    
+    
+}
