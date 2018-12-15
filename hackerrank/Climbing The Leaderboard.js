d21360ec54465c7a75ac328ba55cdd81b8163df9
damiadek <damiadek2009@gmail.com>
2018-12-15 10:30:45 +0100

    more solutions

---
+++ hackerrank/Climbing The Leaderboard.js
@@ -0,0 +1,33 @@
+function climbingLeaderboard(scores, alice) {
+  var ranks = {},
+    lastRank,
+    aliceMin = alice[0],
+    aliceRank = [],
+    index = 1;
+
+  scores.filter(score => score > aliceMin);
+
+  scores.forEach(score => {
+    if (!ranks[score]) {
+      ranks[score] = score;
+    }
+  });
+
+  alice.forEach(score => {
+    if (!ranks[score]) {
+      ranks[score] = score;
+    }
+    index = 1;
+
+    Object.keys(ranks)
+      .sort((a, b) => b - a)
+      .forEach(key => {
+        ranks[key] = index;
+        index += 1;
+      });
+
+    aliceRank.push(ranks[score]);
+  });
+
+  return aliceRank;
+}
